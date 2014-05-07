ae0b91fee95565ecb71db9d0f828382eb075746b
DavidFarrell <dfarrell@davidlearnsgames.com>
2014-05-07 13:29:11 +0200

    Game is technically sort of working.
    sort of

---
+++ FindMeGame/FindMe/Assets/impact_police.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using HutongGames.PlayMaker;
+
+public class impact_police : MonoBehaviour {
+
+
+	public float improve_chance;
+	public bool has_been_enabled = false; 
+
+
+	// Use this for initialization
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("calling impact police update and has been is " + has_been_enabled);
+		if (!has_been_enabled){
+
+			has_been_enabled = true;
+			
+			Debug.Log("calling impact police update and has been is " + has_been_enabled);
+			FsmFloat myFloat = FsmVariables.GlobalVariables.GetFsmFloat("search_chance");
+			myFloat.Value = myFloat.Value + improve_chance;
+		}
+	}
+
+
+}
